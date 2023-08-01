904b9aa03cca63ae175d1f208717d029621f3f7f
Shubhang <shubhangshukla001@gmail.com>
2023-08-02 00:34:45 +0530

    Navigation bar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practice/home_screen.dart';
+import 'package:practice/pages/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +10,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          body: HomeScreen(),
+          body: const HomeScreen(),
+          bottomNavigationBar: BottomNavigationBar(
+              selectedItemColor: Colors.green,
+              unselectedItemColor: Colors.black,
+              items: const [
+                BottomNavigationBarItem(icon: Icon(Icons.store), label: "Shop"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.manage_search), label: "Explore"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.shopping_cart), label: "Cart"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.favorite), label: "Favourite"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.account_box), label: "Account"),
+              ]),
         ));
   }
 }
